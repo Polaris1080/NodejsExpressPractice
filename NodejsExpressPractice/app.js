eac39adb5ac521b96c4bf9c275729d8287deca5c
Polaris1080 <Polaris1080@outlook.jp>
2019-07-12 14:46:15 +0900

    『基本形』を追加

--- NodejsExpressPractice/app.js
+++ NodejsExpressPractice/app.js
@@ -1,3 +1,32 @@
-'use strict';
+/*  Node.js(Express)練習用
+ *
+ *  参考文献：
+ *  https://expressjs.com/ja/
+ *  https://gist.github.com/mitsuruog/fc48397a8e80f051a145
+ *  http://hideyukisaito.github.io/expressjs-doc_ja/guide/
+ */
 
-console.log('Hello world');
+
+
+/*
+ *  おまじない(port3000で起動)
+ */
+const express = require('express')
+const app     = express()
+app.listen(3000)
+
+
+/*
+ *  基本形
+ */
+// GET    http://localhost:3000/foo/
+app.get   ('/foo',         (req, res)=>{ });
+// POST   http://localhost:3000/foo/bar
+app.post  ('/foo/bar',     (req, res)=>{ });
+// PUT    http://localhost:3000/foo/bar/1
+app.put   ('/foo/bar/:id', (req, res)=>{ });
+// DELETE http://localhost:3000/foo/bar/1
+app.delete('/foo/bar/:id', (req, res)=>{ });
+// ALL    http://localhost:3000/foo/
+app.all   ('/foo',         (req, res)=>{ });
+
